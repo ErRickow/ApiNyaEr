558700f76fd54b2d9f5e88bdd977a200f6b92999
Just R <remixingmagelang@gmail.com>
2024-11-17 04:45:59 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ def read_requirements():
 setup(
     name="ApiNyaEr",
     version="1.1",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Er",
     packages=find_packages(),
     install_requires=read_requirements(),
 )
