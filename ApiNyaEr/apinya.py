544b28eb4b440ea202e9f3f0ac54cc15cd5051a2
Just R <remixingmagelang@gmail.com>
2024-11-19 23:56:25 +0700

    update ApiNyaEr/apinya.py

--- ApiNyaEr/apinya.py
+++ ApiNyaEr/apinya.py
@@ -3,6 +3,7 @@ import random
 import string
 from os.path import realpath
 from typing import Union
+from base64 import b64decode as apainier
 
 import aiofiles
 import aiohttp
@@ -79,7 +80,7 @@ class ErApi:
         - num (int): The length of the generated password. Default is 12 if not specified.
 
         Returns:
-        - str: A randomly generated password consisting of characters from string.ascii_letters, string.punctuation, and string.digits.
+        - str: A randomly generated password consisting of characters from string.ascii_letters, string.punctuation, and string.digits.'
         """
         characters = string.ascii_letters + string.punctuation + string.digits
         password = "".join(random.sample(characters, num))
@@ -116,6 +117,52 @@ class ErApi:
         """
         darenya=random.choice(DARE)
         return darenya
+
+    @staticmethod
+    def blackbox(args: str) -> requests.Response:
+        """
+        Interact with the Blackbox AI API for generating content. 🧠
+
+        Args:
+            args (str): The input text to interact with the Blackbox AI chat API.
+
+        Returns:
+            requests.Response: The response object from the API request.
+        """
+
+        url = apainier('aHR0cHM6Ly93d3cuYmxhY2tib3guYWkvYXBpL2NoYXQ=').decode("utf-8")
+        
+        payload = {
+            "agentMode": {},
+            "codeModelMode": True,
+            "id": "XM7KpOE",
+            "isMicMode": False,
+            "maxTokens": None,
+            "messages": [
+                {
+                    "id": "XM7KpOE",
+                    "content": urllib.parse.unquote(args),
+                    "role": "user"
+                }
+            ],
+            "previewToken": None,
+            "trendingAgentMode": {},
+            "userId": "87cdaa48-cdad-4dda-bef5-6087d6fc72f6",
+            "userSystemPrompt": None
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Cookie': 'sessionId=f77a91e1-cbe1-47d0-b138-c2e23eeb5dcf; intercom-id-jlmqxicb=4cf07dd8-742e-4e3f-81de-38669816d300; intercom-device-id-jlmqxicb=1eafaacb-f18d-402a-8255-b763cf390df6; intercom-session-jlmqxicb=',
+            'Origin': apainier('aHR0cHM6Ly93d3cuYmxhY2tib3guYWk=').decode("utf-8"),
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+        }
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            if response.status_code == 200:
+                return {"results": response.text, "join": "@Er_Support_Group", "success": True}
+        except Exception as e:
+            return e  
 
     async def kapan_libur(self):
         """
