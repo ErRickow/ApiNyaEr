5036caeca5578aad9121c6b01e858b8eec01aeb2
Just R <remixingmagelang@gmail.com>
2024-11-19 23:16:53 +0700

    update ApiNyaEr/td.py

--- ApiNyaEr/td.py
+++ ApiNyaEr/td.py
@@ -1,71 +1,12 @@
-from base64 import b64decode as er
+from base64 import b64decode as eranjeng
 
-TRUTH = er[
-    ICAgICJBY2FyYSB0diBhcGEgeWFuZyBwYWxpbmcgbWVtdWFra2FuPyBCZXJpa2FuIGFsYXNhbm55YSEiLCAKICAgICJBcGEgYmFqdSB5YW5nIChtZW51cnV0bXUpIHBhbGluZyBqZWxlayB5YW5nIHBlcm5haCBrYW11IHBha2FpLCBkYW4ga2FwYW4ga2FtdSBtZW1ha2FpbnlhPyIsCiAgICAiQXBhIGJpbmF0YW5nIHBhdHJvbnVzIHlhbmcgY29jb2sgdW50dWsga2FtdT8iLCAiQXBhIGhhbCBwYWxpbmcgYnVydWsgeWFuZyBwZXJuYWgga2FtdSBiaWxhbmcgdGVudGFuZyB0ZW1lbm11PyIsCiAgICAiQXBhIGhhbCBwYWxpbmcgbWVtYWx1a2FuIGRhcmkgZGlyaW11PyIsCiAgICAiQXBhIGhhbCBwYWxpbmcgbWVtYWx1a2FuIGRhcmkgdGVtYW5tdT8iLCAKICAgICJBcGEgaGFsIHBlcnRhbWEgeWFuZyBrYW11IGxpaGF0IHNhYXQga2FtdSBtZWxpaGF0IG9yYW5nIGxhaW4gKGJlZGEgZ2VuZGVyKT8iLAogICAgIkFwYSBoYWwgcGVydGFtYSB5YW5nIHRlcmxpbnRhcyBkaSBwaWtpcmFubXUgc2FhdCBrYW11IG1lbGloYXQgY2VybWluPyIsCiAgICAiQXBhIGhhbCB0ZXJib2RvaCB5YW5nIHBlcm5haCBrYW11IGxha3VrYW4_IiwgIkFwYSBoYWwgdGVyYm9kb2ggeWcgcGVybmFoIGthbXUgbGFrdWthbiA_IiwKICAgICJBcGEga2V0YWt1dGFuIHRlcmJlc2FyIGthbXU_IiwKICAgICJBcGEgbWltcGkgdGVyYnVydWttdT8iLCAiQXBhIG1pbXBpIHRlcmtvbnlvbCB5YW5nIHBlcm5haCBrYW11IGluZ2V0PyIsCiAgICAiQXBhIHBla2VyamFhbiBwYWxpbmcga29ueW9sIHlhbmcgcGVybmFoIGthbXUgYmF5YW5naW4ga2FtdSBha2FuIGphZGk_IiwKICAgICJBcGEgc2lmYXQgdGVyYnVydWttdSBtZW51cnV0IGthbXU_IiwKICAgICJBcGEgc2lmYXQgeWFuZyBpbmdpbiBrYW11IHJ1YmFoIGRhcmkgZGlyaW11PyIsCiAgICAiQXBhIHNpZmF0IHlhbmcgaW5naW4ga2FtdSBydWJhaCBkYXJpIHRlbWFubXU_IiwKICAgICJBcGEgeWFuZyBha2FuIGthbXUgbGFrdWluIGJpbGEgcGFjYXJtdSBiaWxhbmcgaGlkdW5nIGF0YXUgamFyaW11IGplbGVrPyIsCiAgICAiQXBhIHlnIGthbXUgZmlraXJrYW4gc2ViZWx1bSBrYW11IHRpZHVyID8gZXg6IG1lbmdoYXlhbCB0ZW50YW5nIGpvZG9oLGRsbC4iLAogICAgIkFwYWthaCBoYWwgeWFuZyBtZW51cnV0bXUgcGFsaW5nIG1lbm9uam9sIGRhcmkgZGlyaW11PyIsCiAgICAiQmFnaWFuIHR1YnVoIHRlbWFubXUgbWFuYSB5YW5nIHBhbGluZyBrYW11IHN1a2FpIGRhbiBpbmdpbiBrYW11IHB1bnlhPyIsCiAgICAiQmFnaWFuIHR1YnVobXUgbWFuYSB5YW5nIHBhbGluZyBrYW11IGJlbmNpPyIsIAogICAgIkRhcmkgc2VtdWEga2VsYXMgeWFuZyBhZGEgZGkgc2Vrb2xhaCwga2VsYXMgbWFuYSB5YW5nIHBhbGluZyBpbmdpbiBrYW11IG1hc3VraSBkYW4ga2VsYXMgbWFuYSB5YW5nIHBhbGluZyBpbmdpbiBrYW11IGhpbmRhcmk_IiwKICAgICJEZWtzcmlwc2lrYW4gdGVtYW4gdGVyZGVrYXQgbXUhIiwKICAgICJEZXNrcmlwc2lrYW4gZGlyaW11IGRhbGFtIHNhdHUga2F0YSEiLAogICAgIkZpbG0gZGFuIGxhZ3UgYXBhIHlhbmcgcGVybmFoIGJpa2luIGthbXUgbmFuZ2lzPyIsCiAgICAiSGFsIGFwYSB5YW5nIGthbXUgcmFoYXNpYWthbiBzYW1wZSwgc2VrYXJhbmcgZGFuIGdhayBhZGEgc2F0dSBvcmFuZ3B1biB5YW5nIHRhdT8iLAogICAgIkhhbCBwYWxpbmcgcm9tYW50aXMgYXBhIHlhbmcgc2VzZW9yYW5nIChiZWRhIGdlbmRlcikgcGVybmFoIGxha3VpbiBhdGF1IGthc2loIGtlIGthbXU_IiwiSGFsLWhhbCBtZW5qaWppa2FuIGFwYSB5YW5nIHBlcm5haCBrYW11IGFsYW1pID8iLAogICAgIkppa2Ega2FtdSBsYWhpciBrZW1iYWxpIGRhbiBoYXJ1cyBqYWRpIHNhbGFoIHNhdHUgZGFyaSB0ZW1hbm11LCBzaWFwYSB5YW5nIGFrYW4ga2FtdSBwaWxpaCB1bnR1ayBqYWRpIGRpYT8iLAogICAgIkppa2EgcHVueWEga2VrdWF0YW4gc3VwZXIvIHN1cGVyIHBvd2VyIGluZ2luIG1lbGFrdWthbiBhcGEiLAogICAgIkppa2Egc2ViZW50YXIgbGFnaSBraWFtYXQsIGFwYSB5ZyBrYW11IGxha3VrYW4gPyIsCiAgICAiS2FsbyBrYW11IGRpc3VydWggb3BlcmFzaSBwbGFzdGlrIGRlbmdhbiBjb250b2ggd2FqYWggZGFyaSB0ZW1hbiBzZWtlbGFzbXUsIHdhamFoIHNpYXBhIHlhbmcgYWthbiBrYW11IHRpcnU_IiwKICAgICJLYW11IHBlcm5haCBtZW5jdXJpIHNlc3VhdHUgZ2FrPyIsCiAgICAiS2FtdSB0YWt1dCBtYXRpPyBrZW5hcGE_IiwKICAgICJLYXBhbiB0ZXJha2hpciBrYWxpIG1lbmFuZ2lzIGRhbiBtZW5nYXBhPyIsCiAgICAia2VtYW1wdWFuIHNwZXNpYWwga2FtdSBhcGE_IiwKICAgICJLb2sgYmlzYSBzdWthIHNhbWEgb3JhbmcgeWFuZyBrYW11IHN1a2FpPyIsCiAgICAiTWVudXJ1dG11LCBhcGEgc2lmYXQgYmFpayB0ZW1hbiB0ZXJkZWthdG11IHlhbmcgbmdnYWsgZGlhIHNhZGFyaT8iLAogICAgIk9yYW5nIHNlcGVydGkgYXBhIHlhbmcgaW5naW4ga2FtdSBuaWthaGkgc3VhdHUgc2FhdCBuYW50aT8iLAogICAgIlBla2VyamFhbiBwYWxpbmcgbmdlbmVzIGFwYSB5YW5nIG1lbnVydXRtdSBjb2NvayB1bnR1ayB0ZW1hbiBkaSBzZWJlbGFoIGthbmFubXU_IiwKICAgICJQZW5nZW4gdHVrZXJhbiBoaWR1cCBzZWhhcmkgZGVuZ2FuIHNpYXBhPyAodGVtYW4gdGVyZGVrYXQgeWFuZyBrYWxpYW4gc2FtYS1zYW1hIHRhaHUpIGRhbiBtZW5nYXBhIiwKICAgICJQZXJuYWhrYWgga2FtdSBkaWFtLWRpYW0gYmVyaGFyYXAgaHVidW5nYW4gc2VzZW9yYW5nIGRlbmdhbiBwYWNhcm55YSBwdXR1cz8gU2lhcGE_IiwKICAgICJQaWxpaCBQQUNBUiBhdGF1IFRFTUFOID8gYmVyaWthbiBhbGFzYW5ueWEgISIsCiAgICAiUXVvdGUgYXBhIHlhbmcgcGFsaW5nIGthbXUgaW5nYXQgZGFuIGthbXUgc3VrYT8iLAogICAgIlJhaGFzaWEgYXBhIHlnIGJlbHVtIHBlcm5haCBrYW11IGthdGFrYW4gc2FtcGFpIHNla2FyYW5nIGtlcGFkYSB0ZW1hbiBtdSA_IiwKICAgICJSb2xlbW9kZWwgKHBhbnV0YW4pIGthbXUgc2lhcGE_IiwKICAgICJTaWFwYSBkaSBhbnRhcmEgdGVtYW5tdSB5YW5nIGthbXUgcGlraXIgbWF0cmU_IiwKICAgICJTaWFwYSBkaSBhbnRhcmEgdGVtYW4tdGVtYW5tdSB5YW5nIG1lbnVydXRtdSBwb3RvbmdhbiByYW1idXRueWEgcGFsaW5nIG5nZW5lcyAocGFsaW5nIG5nZ2FrIGJhbmdldCk_IiwKICAgICJTaWFwYSBkaWFudGFyYSB0ZW1lbi10ZW1lbm11IHlhbmcgcGFsaW5nIE5HR0FLIGZvdG9nZW5payBkYW4ga2FsbyBkaWZvdG8gbGFnaSBrZXRhd2EgbXVrYW55YSBrYXlhIGt1ZGE_IiwKICAgICJTaWFwYSBtYW50YW4gdGVyaW5kYWggbXU_IGRhbiBtZW5nYXBhIGthbGlhbiBwdXR1cyA_ISIsCiAgICAiU2lhcGEgbmFtYSBhcnRpcyB5YW5nIHBlcm5haCBrYW11IGNpdW0gZm90b255YSBkaWFtLWRpYW0_IiwKICAgICJTaWFwYSBuYW1hIGd1cnUgY293b2sgeWFuZyBwZXJuYWgga2FtdSBzdWthaSBkdWx1PyIsCiAgICAiU2lhcGEgbmFtYSBtYW50YW4gcGFjYXIgdGVtYW4gbXUgeWFuZyBwZXJuYWgga2FtdSBzdWthaSBkaWFtIGRpYW0_IiwKICAgICJTaWFwYSBuYW1hIG9yYW5nIChiZWRhIGdlbmRlcikgeWFuZyBtZW51cnV0bXUgYWthbiBhc3lpayBiaWxhIGRpamFkaWthbiBwYWNhcj8iLAogICAgIlNpYXBhIG5hbWEgb3JhbmcgeWFuZyBrYW11IGJlbmNpLCB0YXBpIGthbXUgcmFzYSBvcmFuZyBpdHUgc3VrYSBzYW1hIGthbXUgKG5nZ2FrIGhhcnVzIGJlZGEgZ2VuZGVyKT8iLAogICAgIlNpYXBhIG5hbWEgb3JhbmcgeWFuZyBwZXJuYWgga2FtdSBpa3V0aW4gZGlhbS1kaWFtPyIsCiAgICAiU2lhcGEgb3JhbmcgKGxhd2FuIGplbmlzKSB5YW5nIHBhbGluZyBzZXJpbmcgdGVybGludGFzIGRpIHBpa2lyYW5tdT8iLAogICAgIlNpYXBhIG9yYW5nIHlnIHBhbGluZyBtZW5qZW5na2Vsa2FuIGRpIGFudGFyYSB0ZW1hbiB0ZW1hbiBtdSA_IGFsYXNhbm55YSEiLAogICAgIlNpYXBhIHNlYmVuZXJueWEgZGkgYW50YXJhIHRlbWFuLXRlbWFubXUgeWFuZyBrYW11IHBpa2lyIGhhcnVzIGRpIG1ha2Utb3Zlcj8iLAogICAgIlNpYXBhIHlhbmcgcGFsaW5nIG1lbmRla2F0aSB0aXBlIHBhc2FuZ2FuIGlkZWFsbXUgZGkgc2luaSI
-].decode("utf-8")
-
-DARE = [
-    "Ajak orang yg tidak kamu kenal itu selfie berdua dengan mu lalu upload ke snapgram",
-    "Ambil beberapa nomor dari kontakmu secara acak dan kirim sms 'Aku hamil' sama mereka.",
-    "Ambil minuman apa saja yg ada didekat mu lalu campurkan dengan cabai dan minum!",
-    "Ambil nomor secara acak dari kontakmu, telepon dia, dan bilang 'Aku mencintaimu'",
-    "Beli makanan paling murah di kantin (atau beli sebotol aqua) dan bilang sambil tersedu-sedu pada teman sekelasmu 'Ini.. adalah makanan yang paling mahal yang pernah kubeli.. Hiks'",
-    "Beli satu botol coca cola dan siram bunga dengan coca cola itu di depan orang banyak.",
-    "Berdiri deket kulkas, tutup mata, pilih makanan secara acak didalemnya, pas makanpun mata harus tetep ditutup.",
-    "Berdiri di tengah lapangan basket dan berteriak, 'AKU MENCINTAIMU PANGERANKU/PUTRIKU'",
-    "Beri hormat pada seseorang di kelas, lalu bilang 'Hamba siap melayani Anda, Yang Mulia.'",
-    "Berjalan sambil bertepuk tangan dan menyanyi lagu 'Selamat Ulang Tahun' dari kelas ke koridor.",
-    "Berlutut satu kaki dan bilang 'Marry me?' sama orang pertama yang masuk ke ruangan.",
-    "Bikin hiasan kepala absurd dari tisu, apapun itu, terus suruh pose didepan kamera, terus upload",
-    "Bilang 'KAMU CANTIK BANGET NGGAK BOHONG' sama cewek yang menurutmu paling cantik di kelas ini",
-    "Bilang pada seorang guru, 'Bu/Pak, baju saya terasa sempit' dengan ekspresi memelas.",
-    "Bilang pada seseorang di kelas, 'Aku baru saja diberi tahu aku adalah kembaranmu dulu, kita dipisahkan, lalu aku menjalani operasi plastik. Dan ini adalah hal paling serius yang pernah aku katakan.'",
-    "Buang buku catatan seseorang ke tempat sampah, di depan matanya, sambil bilang 'Buku ini isinya tidak ada yang bisa memahami'",
-    "Cabut bulu kaki mu sendiri sebanyak 3x",
-    "Chat kedua orangtuamu, katakan bahwa kamu kangen dengan mereka lengkap dengan emoticon sedih.",
-    "Coba searcing google mengenai hal-hal yang mengerikan atau menggelikan seperti trypophobia, dll.",
-    "Duduk relaks di tengah lapangan basket sambil berpura-pura itu adalah pantai untuk berjemur.",
-    "isi mulut penuh dengan air dan harus tahan hingga dua putaran. Jika tertawa dan tumpah atau terminum, maka harus ngisi ulang dan ditambah satu putaran lagi.",
-    "Jabat tangan orang pertama yang masuk ke ruangan ini dan bilang 'Selamat datang di Who Wants To Be a Millionaire!'",
-    "Kirim sms pada orangtuamu 'Hai, bro! Aku baru beli majalah Playboy edisi terbaru!'",
-    "Kirim sms pada orangtuamu, 'Ma, Pa, aku sudah tahu bahwa aku adalah anak adopsi dari Panti Asuhan. Jangan menyembunyikan hal ini lagi.'",
-    "Kirim sms pada tiga nomor acak di kontakmu dan tulis 'Aku baru saja menjadi model majalah Playboy.'",
-    "Makan 1 Sendok makan kecap manis dan kecap asin!",
-    "Makan sesuatu tapi gak pake tangan.",
-    "Marah-marah ketemen kamu yang gak dateng padahal udah janjian mau main 'truth or dare' bareng-bareng",
-    "Mecahin telur pake kepala.",
-    "Memakan makanan yang sudah dicampur-campur dan rasanya pasti aneh, namun pastikan bahwa makanan itu tidak berbahaya untuk kesehatan jangka panjang maupun jangka pendek.",
-    "Menari ala Girls' Generation untuk cowok di depan kelas, atau menari ala Super Junior untuk cewek.",
-    "Mengerek tiang bendera tanpa ada benderanya.",
-    "Menggombali orang yang ditaksir, sahabat terdekat, lawan jenis yang tidak dikenal sama sekali dan  sejenisnya.",
-    "Meniru style rambut semua temen kamu.",
-    "Menyanyikan lagu HAI TAYO di depan banyak orang sambil menari",
-    "Menyanyikan lagu Iwak Peyek dengan keras di ruang kelas.",
-    "Minjem sesuatu ke tetangga",
-    "Minta tandatangan pada seorang guru yang paling kamu benci sambil bilang 'Anda benar-benar orang yang paling saya kagumi di dunia.'",
-    "Minta uang pada seseorang (random/acak) di jalan sambil bilang 'Saya tidak punya uang untuk naik angkot.'",
-    "Minum sesuatu yang udah dibuat/disepakatin, tapi pastiin gak berbahaya, bisa kayak minum sirup yang digaremin terus ditambah kecap.",
-    "Minum tiga teguk teh atau coke (coca-cola atau sprite) yang dicampur sambal.",
-    "Ngomong ke gebetannya emoticon-Takut, ngobrol ngalurngidul apapun lah boleh ,via manapun juga bisa.",
-    "Nyanyi-nyanyi lagu favorit difilm disney diluar rumah sambil teriak-teriak.",
-    "Nyebutin 1 biru sampai 10 biru dengan cepat dan tidak boleh melakukan kesalahan. Jika salah maka harus diulang dari awal.",
-    "Pakai mahkota tiruan dari kertas buku dan bilang sama setiap orang di ruangan 'BERI PENGHORMATAN PADA YANG MULIA RAJA' sambil menunjuk setiap orang dengan penggaris.",
-    "Pake celana kebalik sampe besok paginya.",
-    "Pegang bola basket, berdiri di depan kelas, dan berteriak, 'ADA YANG TAHU MENGAPA BOLA GOLF INI SANGAT BESAR? APA PABRIKNYA SALAH CETAK?'",
-    "Peluk orang yang NGGAK kamu sukai di kelas dan bilang, 'Terimakasih banyak kamu sudah bersedia menjadi orang paling baik untukku.'",
-    "Pergi ke lapangan yg luas, lalu berlari sekencang kencangnya sambil mengatakan 'aku gila aku gila'",
-    "Petik 1 bunga lalu tancapkan bunga itu ke orang yg tidak kamu kenal (harus lawan jenis ya)",
-    "Pilih orang secara acak di jalan, lalu bilang 'You don't know you're beautiful' (ala One Direction)",
-    "Pura pura kerasukan ex: kerasukan macan dll",
-    "Suruh bersiul pas mulutnya lagi penuh dijejelin makanan.",
-    "Suruh jadi pelayan buat ngelayanin kamu sama temen-temen kamu buat makan siang.",
-    "Suruh pake kaos kaki buat dijadiin sarung tangan.",
-    "Suruh pake topi paling aneh/helm paling absurd selama 3 putaraann kedepan.",
-    "Telpon mama kamu dan bilang 'ma, aku mau nikah secepatnya'",
-    "Telpon mantan kamu dan bialng 'aku rindu kamu'",
-    "Teriak 'WOI GW JACK, DENGER NIH RAUNGAN GW, ROAAAAR!' ditempat rame",
-    "Tuker baju sama orang terdekat sampe ronde berikutnya.",
-    "Update status di BBM, Line, WA, atau apapun itu dengan kata kata yang semuanya berawalan 'T'",
-    "Upload video dia nyanyi ke youtube yang lagi nyanyiin lagu-lagu populer",
-    "Warnain kuku kaki dan tangan tapi dengan warna berbeda-beda buat seminggu",
-]
+def truth():
+    try:
+        huh = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL25heWExNTAzL3dhcm5pbmcvbWFpbi9kZXZzLmpzb24="
+        bb = eranjeng(huh).decode("utf-8")
+        res = requests.get(bb)
+        if res.status_code == 200:
+            return json.loads(res.text)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+        sys.exit(1)
