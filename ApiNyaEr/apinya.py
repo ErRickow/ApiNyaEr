8ddd1ab9142ea526e9495faaaee20713ad014dc8
Just R <remixingmagelang@gmail.com>
2024-11-20 14:16:55 +0700

    update ApiNyaEr/apinya.py

--- ApiNyaEr/apinya.py
+++ ApiNyaEr/apinya.py
@@ -672,7 +672,7 @@ class ErApi:
             - keywords (str): Kata kunci yang terkait dengan paket.
             - classifiers (list): Daftar pengklasifikasi PyPI.
             - project_urls (dict): URL proyek tambahan (misalnya, kode sumber, dokumentasi).
-            Returns None jika paket tidak ditemukan atau terjadi kesalahan.
+         Returns None jika paket tidak ditemukan atau terjadi kesalahan.
         """
         url = f"{self.base_urls['pypi']}/{namanya}/json"
         response = await self._make_request(url)
