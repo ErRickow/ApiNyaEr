ac5fff7970eff01fda5c382daf4e76bccae3766a
Just R <remixingmagelang@gmail.com>
2024-12-29 02:49:38 +0700

    update ApiNyaEr/fungsi.py

--- ApiNyaEr/fungsi.py
+++ ApiNyaEr/fungsi.py
@@ -1,6 +1,6 @@
 class FilePath(str):
     """
-    A wrapper around a file path string that provides an additional delete method. hm
+    A wrapper around a file path string that provides an additional delete method.
 
     Attributes:
         path (str): The file path to the media file.
