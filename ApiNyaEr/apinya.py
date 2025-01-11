6a6ac948f5c5d46f97f2507d2105d06416aa6373
Just R <remixingmagelang@gmail.com>
2025-01-12 04:28:36 +0700

    update ApiNyaEr/apinya.py

--- ApiNyaEr/apinya.py
+++ ApiNyaEr/apinya.py
@@ -20,9 +20,7 @@ from .teks import ANIMEK, EPEP, FAKTA, HECKER, ISLAMIC, PUBG
 class ErApi:
     def __init__(self):
         self.base_urls = {
-            "luminai": apainier(
-                "aHR0cHM6Ly9yZXN0LWVyLWFwaS52ZXJjZWwuYXBwL2x1bWluYWk="
-            ).decode("utf-8"),
+            "luminai": apainier("aHR0cHM6Ly9yZXN0LWVyLWFwaS52ZXJjZWwuYXBwL2x1bWluYWk=").decode("utf-8"),
             "pinter": "https://api.ryzendesu.vip/api/search/pinterest?query={query}",
             "neko_url": apainier(
                 "aHR0cHM6Ly9uZWtvcy5iZXN0L2FwaS92Mi97ZW5kcG9pbnR9P2Ftb3VudD17YW1vdW50fQ=="
@@ -376,7 +374,7 @@ class ErApi:
             response = requests.post(url, json=payload, headers=headers)
             if response.status_code == 200:
                 return {
-                    "results": response.text,
+                    "results": response.text.strip(),
                     "join": "@Er_Support_Group",
                     "success": True,
                 }
@@ -398,15 +396,13 @@ class ErApi:
         """
         Args:
             tanya (str): Teks query
-
+    
         Returns:
             resultnya
         """
         params = {"text": tanya}
         try:
-            response = await self._make_request(
-                self.base_urls["luminai"], params=params
-            )
+            response = await self._make_request(self.base_urls["luminai"], params=params)
             if response["data"]:
                 return {
                     "resultnya": response["data"]["result"],
