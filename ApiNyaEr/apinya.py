ac91d87d541fc0d1e01a70c5996342fec6f8f27e
Just R <remixingmagelang@gmail.com>
2025-01-13 22:01:13 +0700

    update ApiNyaEr/apinya.py

--- ApiNyaEr/apinya.py
+++ ApiNyaEr/apinya.py
@@ -20,15 +20,10 @@ from .teks import ANIMEK, EPEP, FAKTA, HECKER, ISLAMIC, PUBG
 class ErApi:
     def __init__(self):
         self.base_urls = {
-            "hehe": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvbG9nb21ha2Vy").decode(
-                "utf-8"
-            ),
-            "whe": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvaXNsYW1haQ==").decode(
-                "utf-8"
-            ),
-            "njir": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvdGVyYWJveA==").decode(
-                "utf-8"
-            ),
+            "ai": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvb3BlbmFp").decode("utf-8"),
+            "hehe": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvbG9nb21ha2Vy").decode("utf-8"),
+            "whe": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvaXNsYW1haQ==").decode("utf-8"),
+            "njir": apainier("aHR0cHM6Ly92YXBpcy5teS5pZC9hcGkvdGVyYWJveA==").decode("utf-8"),
             "luminai": apainier(
                 "aHR0cHM6Ly9yZXN0LWVyLWFwaS52ZXJjZWwuYXBwL2x1bWluYWk="
             ).decode("utf-8"),
@@ -413,7 +408,9 @@ class ErApi:
         """
         params = {"url": link}
         try:
-            response = await self._make_request(self.base_urls["njir"], params=params)
+            response = await self._make_request(
+                self.base_urls["njir"], params=params
+            )
             if response["data"]:
                 return {
                     "judul": response["data"]["filename"],
@@ -429,13 +426,14 @@ class ErApi:
         """
         args:
             tanya (str): teks pertanyaan
-
+            
         Returns:
             resultnya
         """
         paman = {"q": tanya}
         try:
-            res = await self._make_request(self.base_urls["whe"], params=paman)
+            res = await self._make_request(self.base_urls["whe"], params=paman
+            )
             if res["status"] == True:
                 return {
                     "resultnya": res["result"],
@@ -449,7 +447,7 @@ class ErApi:
     async def logo_maker(self, input: str):
         """
         Membuat Logo Dari Input yang di masukkan
-
+        
         Args:
             input: teks yang akan di buat Logo
         Returns:
@@ -461,7 +459,7 @@ class ErApi:
             res = await self._make_request(url, params=parang)
             return res
         except Exception as r:
-            return r
+          return r
 
     async def luminai(self, tanya: str):
         """
@@ -496,12 +494,18 @@ class ErApi:
         Returns:
         str: Respon chatbotnya.
         """
-        x = apainier(
-            "aHR0cHM6Ly9mYWxsZW54Ym90LnZlcmNlbC5hcHAvYXBpL2FwaWtleT01OTM1NjA4Mjk3LWZhbGxlbi11c2JrMzNrYnN1L2dyb3VwLWNvbnRyb2xsZXIvbXVrZXNoL21lc3NhZ2U9"
-        ).decode("utf-8")
-        full_url = f"{x}{tanya}"
-        response = requests.get(full_url).json()["reply"]
-        return response
+        url = self.base_urls["ai"]
+        par = {"q": tanya}
+        try:
+            res = self._make_request(url, params=par)
+            if res["status"] == True:
+                return {
+                    "resultnya": res["result"],
+                    "from": "ApiNyaEr",
+                    "join": "@Er_Support_Group"
+                }
+        except Exception as er:
+            return er
 
     async def doa(self, nama_doa: str) -> str:
         """
