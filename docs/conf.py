8b8206916f951d5840a1b944d76a26cce8db6c61
Just R <remixingmagelang@gmail.com>
2025-01-16 10:59:56 +0700

    update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,39 +1,32 @@
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath("../ApiNyaEr"))
-project = "ApiNyaEr"
-copyright = "2025, ErNewDev0"
-author = "ErRickow"
-release = "1.4"
+sys.path.insert(0, os.path.abspath('...'))
+project = 'ApiNyaEr'
+copyright = '2025, ErNewDev0'
+author = 'ErRickow'
+release = '1.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.autodoc",
-    "sphinx_copybutton",
-    "sphinx_rtd_dark_mode",
-]
+extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc","sphinx_copybutton","sphinx_rtd_dark_mode"]
 
 default_dark_mode = True
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-latex_engine = "xelatex"
+latex_engine = 'xelatex'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 autosectionlabel_prefix_document = True
 hoverxref_auto_ref = True
 hoverxref_roles = [
-    "term",
+    'term',
 ]
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ['_static']
 html_logo = "logo.png"
-html_show_sphinx = False
-html_show_copyright = False
+html_show_sphinx=False
+html_show_copyright=False
 copybutton_prompt_text = ">>> "
