86c5b2f85fd1bb252b3b4c0337f3c178c2ed0c95
Just R <remixingmagelang@gmail.com>
2025-01-16 10:02:03 +0700

    update ApiNyaEr/apinya.py

--- ApiNyaEr/apinya.py
+++ ApiNyaEr/apinya.py
@@ -127,81 +127,82 @@ class ErApi:
         """
         Fetch spesifik Gambar/Gif Anime.
 
-         Args:
-             endpoint (str): Kategori endpoin gambar/Gif animenya. Defaultnya "kiss".
-                 Valid Format endpoints:
-                 - "husbando", "kitsune", "neko", "waifu"
-                 Valid GIF endpoints:
-                 - "baka", "bite", "blush", "bored", "cry", "cuddle", "dance", "facepalm",
-                   "feed", "handhold", "handshake", "happy", "highfive", "hug", "kick",
-                   "kiss", "laugh", "lurk", "nod", "nom", "nope", "pat", "peck", "poke",
-                   "pout", "punch", "shoot", "shrug", "slap", "sleep", "smile", "smug",
-                   "stare", "think", "thumbsup", "tickle", "wave", "wink", "yawn", "yeet"
-             amount (int): jumlah item gambarnya. Default 1.
+        Args:
+          endpoint (str): Kategori endpoin gambar/Gif animenya. Defaultnya "kiss".
+              Valid Format endpoints:
+              - "husbando", "kitsune", "neko", "waifu"
+              Valid GIF endpoints:
+              - "baka", "bite", "blush", "bored", "cry", "cuddle", "dance", "facepalm",
+                "feed", "handhold", "handshake", "happy", "highfive", "hug", "kick",
+                "kiss", "laugh", "lurk", "nod", "nom", "nope", "pat", "peck", "poke",
+                "pout", "punch", "shoot", "shrug", "slap", "sleep", "smile", "smug",
+                "stare", "think", "thumbsup", "tickle", "wave", "wink", "yawn", "yeet"
 
-         Returns:
-             dict: Dictionary konten yang di request. Dictionarynya memiliki kata kunci `"results"`, yang menampung limit.
+          amount (int): jumlah item gambarnya. Default 1.
 
-         Raises:
-             ValueError: Jika endpoint tidak valid.
+        Returns:
+          dict: Dictionary konten yang di request. Dictionarynya memiliki kata kunci `"results"`, yang menampung limit.
+
+        Raises:
+          ValueError: Jika endpoint tidak valid.
         """
-        valid_categories = [
-            "husbando",
-            "kitsune",
-            "neko",
-            "waifu",  # Images
-            "baka",
-            "bite",
-            "blush",
-            "bored",
-            "cry",
-            "cuddle",
-            "dance",
-            "facepalm",
-            "feed",
-            "handhold",
-            "handshake",
-            "happy",
-            "highfive",
-            "hug",
-            "kick",
-            "kiss",
-            "laugh",
-            "lurk",
-            "nod",
-            "nom",
-            "nope",
-            "pat",
-            "peck",
-            "poke",
-            "pout",
-            "punch",
-            "shoot",
-            "shrug",
-            "slap",
-            "sleep",
-            "smile",
-            "smug",
-            "stare",
-            "think",
-            "thumbsup",
-            "tickle",
-            "wave",
-            "wink",
-            "yawn",
-            "yeet",  # GIFs
-        ]
-
-        if endpoint not in valid_categories:
-            raise ValueError(
-                f"SALAH GUOBLOK '{endpoint}'. Harus yang kek gini: {', '.join(valid_categories)}"
-            )
-
-        url = self.base_urls["neko_url"].format(endpoint=endpoint, amount=amount)
-
-        response = await self._make_request(url)
-
-        return response
+            valid_categories = [
+                "husbando",
+                "kitsune",
+                "neko",
+                "waifu",  # Images
+                "baka",
+                "bite",
+                "blush",
+                "bored",
+                "cry",
+                "cuddle",
+                "dance",
+                "facepalm",
+                "feed",
+                "handhold",
+                "handshake",
+                "happy",
+                "highfive",
+                "hug",
+                "kick",
+                "kiss",
+                "laugh",
+                "lurk",
+                "nod",
+                "nom",
+                "nope",
+                "pat",
+                "peck",
+                "poke",
+                "pout",
+                "punch",
+                "shoot",
+                "shrug",
+                "slap",
+                "sleep",
+                "smile",
+                "smug",
+                "stare",
+                "think",
+                "thumbsup",
+                "tickle",
+                "wave",
+                "wink",
+                "yawn",
+                "yeet",  # GIFs
+            ]
+    
+            if endpoint not in valid_categories:
+                raise ValueError(
+                    f"SALAH GUOBLOK '{endpoint}'. Harus yang kek gini: {', '.join(valid_categories)}"
+                )
+    
+            url = self.base_urls["neko_url"].format(endpoint=endpoint, amount=amount)
+    
+            response = await self._make_request(url)
+    
+            return response
 
     @staticmethod
     def password(num: int = 12) -> str:
@@ -730,20 +731,20 @@ class ErApi:
         Pencarian GitHub untuk beberapa tipe konten.
 
         Args:
-            cari (str): untuk Pencarian.
-            tipe (str, optional): Type pencarian, terdiri dari:
-                - "repositories"
-                - "users"
-                - "organizations"
-                - "issues"
-                - "pull_requests"
-                - "commits"
-                - "topics"
-                Defaults ke "repositories".
-            max_results (int, optional): Maximum nomor dari results untuk return. Defaultnya 3.
+          cari (str): untuk Pencarian.
+          tipe (str, optional): Type pencarian, terdiri dari:
+              - "repositories"
+              - "users"
+              - "organizations"
+              - "issues"
+              - "pull_requests"
+              - "commits"
+              - "topics"
+              Defaults ke "repositories".
+              max_results (int, optional): Maximum nomor dari results untuk return. Defaultnya 3.
 
         Returns:
-            list: List dari pencarian results atau pesan error.
+          list: List dari pencarian results atau pesan error.
         """
         tipe_yang_valid = [
             "repositories",
